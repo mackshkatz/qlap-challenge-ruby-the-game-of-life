9998e1094f63f4d66c506bc881cf49325f933a0b
Max Katz <maxwelljkatz@gmail.com>
2012-01-20 15:54:15 -0700

    At this point, I need to figure out the logic to determine whether each cell is dead or a live. I then need to set @life to true or false.

--- app.rb
+++ app.rb
@@ -1,25 +1,51 @@
 class World
 	def initialize()
-
+		@main_world = new_matrix()
 	end
 
-	def beginWorld
-		world = ""
-		@m = []
+	def new_matrix
+		#return 100x100 matrix
+		m = []
 
 		100.times do |x|
-			@m[x] = []
+			m[x] = []
 
 			100.times do |y|
-				@m[x][y] = 0
-				world += y.to_s
+				m[x][y] = Cell.new(x,y)
 			end
-
-			world += "\n"
 		end
 
-		
+		return m
 	end
+
+	def begin_world
+		# get empty matrix
+		100.times do
+			next_gen()
+		end
+
+	end
+
+	def print_matrix
+		puts @main_world
+	end
+
+	def update_matrix
+		new_world = new_matrix()
+		@main_world.each do |x|
+			x.each do |y|
+				@main_world[x][y].alive(@main_world)
+				new_world[x][y] = @main_world[x][y]
+			end
+		end
+		@main_world = new_world
+	end
+
+	def next_gen
+		print_matrix()
+		update_matrix()
+	end
+
 
 	def nextFrame
 		#iterate through each element and determine
@@ -29,7 +55,11 @@ class World
 		# 		  #{f[x+1][y-1]} + #{f[x-1][y]} + #{f[x+1][y]} +
 		# 		  #{f[x-1][y+1]} + #{f[x][y+1]} + #{f[x+1][y+1]}
 		@m.each do |x|
-			x.each do |y|		
+			x.each do |y|
+				if y == 1
+
+				end
+					
 				# if y == 1
 				# 	if score < 2
 				# 		y = 0
@@ -49,13 +79,20 @@ class World
 end
 
 
-class Cell < World
-	def dead
-
+class Cell
+	def initialize(x,y)
+		@x = x
+		@y = y
+		@life = false
 	end
 
-	def alive
+	def update_life(matrix)
+		matrix[@x][@y]
+		# depending on score, set @life to true or false
+	end
 
+	def alive?
+		@life
 	end
 end
 

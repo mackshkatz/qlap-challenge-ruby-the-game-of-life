b49da8b96e059db1a783c1c6b23afb768762427a
Max Katz <maxwelljkatz@gmail.com>
2012-01-20 17:12:20 -0700

    Added a call to update_life in the update_matrix method.

--- app.rb
+++ app.rb
@@ -23,7 +23,11 @@ class World
 		100.times do
 			next_gen()
 		end
+	end
 
+	def next_gen
+		print_matrix()
+		update_matrix()
 	end
 
 	def print_matrix
@@ -34,30 +38,12 @@ class World
 		new_world = new_matrix()
 		@main_world.each do |x|
 			x.each do |y|
-				@main_world[x][y].alive(@main_world)
+				@main_world.update_life(@main_world)
+				@main_world[x][y].alive?
 				new_world[x][y] = @main_world[x][y]
 			end
 		end
 		@main_world = new_world
-	end
-
-	def next_gen
-		print_matrix()
-		update_matrix()
-	end
-
-
-	def nextFrame
-		#iterate through each element and determine
-		# whether cell is dead or alive
-
-		@m.each do |x|
-			x.each do |y|
-				if y == 1
-
-				end
-			end
-		end
 	end
 end
 
@@ -72,7 +58,9 @@ class Cell
 	def update_life(matrix)
 		matrix[@x][@y]
 		# depending on score, set @life to true or false
-		score = matrix[@x-1][@y-1] + matrix[@x][@y-1] + matrix[@x+1][@y-1] + matrix[@x-1][@y] + matrix[@x+1][@y] + matrix[@x-1][@y+1] + matrix[@x][@y+1] + matrix[@x+1][@y+1]
+		score = matrix[@x-1][@y-1] + matrix[@x][@y-1] + matrix[@x+1][@y-1] + 
+				matrix[@x-1][@y] + matrix[@x+1][@y] + matrix[@x-1][@y+1] + 
+				matrix[@x][@y+1] + matrix[@x+1][@y+1]
 		if score < 2
 			@life = false
 			#matrix[@x][@y] = 0
@@ -89,10 +77,24 @@ class Cell
 	end
 
 	def alive?
-		@life
+		return @life
 	end
 end
 
-w1 = World.new 
-w1.beginWorld
+w1 = World.new
+# I instantiate the class to create a new World object.
+# The initialize method immediately declares the @main_world
+# instance variable, calls the new_matrix method and sets the
+# instance variable to the return value, which is a multi-
+# dimensional array 100x100.
+w1.begin_world()
+# This is called simply to tell the next_gen method to run 100 times.
+# 1. The next_gen method first calls the print_matrix method. This simply
+# 'puts' the matrix being stored in the @main_world instance variable.
+#  2. The next_gen method then calls the update_matrix method. This
+# first declares a new variable new_world and assigns the return value
+# of the new_matrix method to it, which is just a 100x100 multi-
+# dimensional array. Update_matrix then iterates over each row by iterating
+# over each item in the array, where each item is also an array, so it then
+# iterates over this inner array. 
 w1.nextFrame()
